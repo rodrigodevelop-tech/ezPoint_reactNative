944dd6163b0fd9da1496e115b4775d0ccaba845d
viniciusczar <viniciusczar@outlook.com>
2021-06-03 00:28:30 -0300

    Gradient das widgets iniciais

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -4,52 +4,67 @@ import {
     SafeAreaView,
     StyleSheet,
     Text,
-    View
+    View,
+    TouchableOpacity
 } from 'react-native';
 
 import { Button } from '../components/Button';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 
 
-export function Confirmation(){
+export function Confirmation() {
     const navigation = useNavigation();
 
 
-    function handleConfirmation(){
+    function handleConfirmation() {
         navigation.navigate('UserLogin');
     }
-    
-    return(
-       <SafeAreaView style={styles.container}>
-            <View style={styles.content}>
 
-                <Text style={styles.emoji}>
-                    😃
+    return (
+        <SafeAreaView style={styles.container}>
+            <LinearGradient
+                colors={[colors.azul_marinho, colors.cinza]}
+                style={styles.linearGradientBackGround}
+            >
+                <View style={styles.content}>
+
+                    <Text style={styles.emoji}>
+                        😃
                 </Text>
 
-                <Text style={styles.title}>
-                    Finalizado
+                    <Text style={styles.title}>
+                        Finalizado
                 </Text>
 
-                <Text style={styles.subTitle}>
-                    Agora vamos começar a {'\n'}
-                    organizar suas tarefas. 
+                    <Text style={styles.subTitle}>
+                        Agora vamos começar a {'\n'}
+                    organizar suas tarefas.
                 </Text>
 
-                <View style={styles.footer}>
-                    <Button
-                        title="Finalizar"
-                        onPress={handleConfirmation}
-                    />
+                    <View style={styles.footer}>
+                        <TouchableOpacity
+                            style={styles.button}
+                            activeOpacity={0.70}
+                            onPress={handleConfirmation}
+                        >
+                            <LinearGradient
+                                colors={[colors.azul_black, colors.blue, colors.azul_black]}
+                                style={styles.gradientButton}>
+                                <Text
+                                    style={styles.confirmText}
+                                >Finalizar</Text>
+                            </LinearGradient>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
 
-           </View>
-
-           
-       </SafeAreaView> 
+            </LinearGradient>
+        </SafeAreaView>
     )
 }
 
@@ -60,7 +75,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     content: {
-        flex:1,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
@@ -77,17 +92,46 @@ const styles = StyleSheet.create({
         lineHeight: 38,
         marginTop: 15
     },
-    subTitle:{
+    subTitle: {
         fontFamily: fonts.text,
         textAlign: 'center',
         fontSize: 17,
-        paddingVertical:20,
+        paddingVertical: 20,
         color: colors.heading
     },
     footer: {
         width: '100%',
-        paddingHorizontal:50,
+        paddingHorizontal: 50,
         marginTop: 20
+    },
+
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        height: 56,
+        width: 200
+    },
+
+    linearGradientBackGround: {
+        flex: 1,
+        paddingLeft: 40,
+        paddingRight: 40,
+        borderRadius: 5
+    },
+
+    confirmText: {
+        fontSize: 20,
+        fontFamily: fonts.text,
+        color: 'white',
+        marginTop: 13
+    },
+
+    gradientButton: {
+        flex: 1,
+        paddingLeft: 50,
+        paddingRight: 50,
+        borderRadius: 10
     },
 
 })
--- src/pages/UserLogin.tsx
+++ src/pages/UserLogin.tsx
@@ -8,7 +8,8 @@ import {
     KeyboardAvoidingView,
     Platform,
     Image,
-    Dimensions
+    Dimensions,
+    TouchableOpacity
 } from 'react-native';
 
 
@@ -16,17 +17,18 @@ import { useNavigation } from '@react-navigation/core';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-import { AntDesign  } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 import { Button } from '../components/Button';
+import { LinearGradient } from 'expo-linear-gradient';
 import Logo from '../assets/EZPOINT_semTitulo.png';
 
 
 
-export function UserLogin(){
-    const [isFocusedEmail,setIsFocusedEmail] = useState(false);
-    const [isFilledEmail,setIsFilledEmail] = useState(false);
-    const [isFocusedPwd,setIsFocusedPwd] = useState(false);
-    const [isFilledPwd,setIsFilledPwd] = useState(false);
+export function UserLogin() {
+    const [isFocusedEmail, setIsFocusedEmail] = useState(false);
+    const [isFilledEmail, setIsFilledEmail] = useState(false);
+    const [isFocusedPwd, setIsFocusedPwd] = useState(false);
+    const [isFilledPwd, setIsFilledPwd] = useState(false);
     const [nameEmail, setNameEmail] = useState<string>();
     const [namePwd, setNamePwd] = useState<string>();
     const [typeInput, seTypeInput] = useState<string>();
@@ -35,125 +37,139 @@ export function UserLogin(){
 
     const navigation = useNavigation();
 
-    function handleUserLogin(){
+    function handleUserLogin() {
         navigation.navigate("Dashboard");
     }
 
-    function handleInputBlur(typeInput:string){
-        if(typeInput==="email"){
+    function handleInputBlur(typeInput: string) {
+        if (typeInput === "email") {
             setIsFocusedEmail(false);
             setIsFilledEmail(!!nameEmail);
             seTypeInput("email");
         }
-        if(typeInput==="pwd"){
+        if (typeInput === "pwd") {
             setIsFocusedPwd(false);
             setIsFilledPwd(!!namePwd);
             seTypeInput("pwd");
         }
-       
+
     }
 
-    function handleInputFocus(typeInput:string){
-        if(typeInput==="email"){
+    function handleInputFocus(typeInput: string) {
+        if (typeInput === "email") {
             setIsFocusedEmail(true);
             seTypeInput("email");
         }
-           
 
-        if(typeInput==="pwd"){
+
+        if (typeInput === "pwd") {
             setIsFocusedPwd(true);
             seTypeInput("pwd");
         }
-           
+
     }
 
-    function handleInputChange(value:string){
-        if(typeInput==="email"){
+    function handleInputChange(value: string) {
+        if (typeInput === "email") {
             setIsFilledEmail(!!value);
             setNameEmail(value);
         }
-        if(typeInput==="pwd"){
+        if (typeInput === "pwd") {
             setIsFilledPwd(!!value);
             setNamePwd(value);
         }
     }
 
 
-    return(
+    return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView
                 style={styles.container}
                 behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             >
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <Image 
-                            source={Logo} 
-                            style={styles.image}
-                            resizeMode='contain'
-                        />
-                        <Text style={styles.title}>
-                            Faça o seu login
+                <LinearGradient
+                    colors={[colors.azul_marinho, colors.cinza]}
+                    style={styles.linearGradientBackGround}
+                >
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <Image
+                                source={Logo}
+                                style={styles.image}
+                                resizeMode='contain'
+                            />
+                            <Text style={styles.title}>
+                                Faça o seu login
                         </Text>
-                        
+
                             <Text style={styles.subTitle}>
                                 Email
                             </Text>
-                            
+
                             <TextInput
                                 style={[
                                     styles.input,
                                     (isFocusedEmail || isFilledEmail) &&
-                                    {borderColor: colors.green}
+                                    { borderColor: colors.green }
                                 ]}
                                 placeholder="Digite seu email"
-                                onBlur={()=>handleInputBlur("email")}
-                                onFocus={()=> handleInputFocus("email")}
+                                onBlur={() => handleInputBlur("email")}
+                                onFocus={() => handleInputFocus("email")}
                                 onChangeText={handleInputChange}
                                 textContentType="password"
-                                
+
                             />
-                            
-                  
+
+
                             <Text style={styles.subTitle}>
-                              Senha
+                                Senha
                             </Text>
-                            <View style={styles.viewInput}>    
+                            <View style={styles.viewInput}>
                                 <TextInput
                                     style={[
                                         styles.input,
                                         (isFocusedPwd || isFilledPwd) &&
-                                        {borderColor: colors.green}
+                                        { borderColor: colors.green }
                                     ]}
                                     placeholder="Digite uma senha"
-                                    onBlur={()=>handleInputBlur("pwd")}
-                                    onFocus={()=> handleInputFocus("pwd")}
+                                    onBlur={() => handleInputBlur("pwd")}
+                                    onFocus={() => handleInputFocus("pwd")}
                                     onChangeText={handleInputChange}
                                     secureTextEntry={secure}
                                 />
-                                <AntDesign 
-                                    name="eye" 
+                                <AntDesign
+                                    name="eye"
                                     style={styles.iconEye}
-                                    onPress={()=>setSecure(!secure)}
+                                    onPress={() => setSecure(!secure)}
                                 />
                             </View>
                             <View style={styles.button}>
-                                <Button
-                                    title="Entrar"
+                                <TouchableOpacity
+                                    style={styles.button}
+                                    activeOpacity={0.70}
                                     onPress={handleUserLogin}
-                                />
+                                >
+                                    <LinearGradient
+                                        colors={[colors.azul_black, colors.blue, colors.azul_black]}
+                                        style={styles.gradientButton}>
+                                        <Text
+                                            style={styles.confirmText}
+                                        >Continuar</Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>
                             </View>
-                       
+
+                        </View>
                     </View>
-                </View>
+                </LinearGradient>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
         width: '100%',
 
         alignItems: 'center',
@@ -164,17 +180,17 @@ const styles = StyleSheet.create({
         width: '100%'
     },
     form: {
-        flex:1,
+        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
         paddingHorizontal: 54,
+        alignItems: 'center',
     },
     image: {
-        height:Dimensions.get('window').width * 0.7 
+        height: Dimensions.get('window').width * 0.7
     },
     title: {
         fontSize: 24,
-        lineHeight:32,
+        lineHeight: 32,
         textAlign: 'center',
         color: colors.heading,
         fontFamily: fonts.heading,
@@ -195,25 +211,47 @@ const styles = StyleSheet.create({
         width: '100%',
         fontSize: 15,
         marginTop: 2,
-        padding: 10,
         // paddingLeft:0,
         textAlign: 'left'
     },
-    viewInput:{
+    viewInput: {
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
     },
-    iconEye:{
-        fontSize:18,
-        color: colors.azul_black, 
-        padding:10,
-        marginLeft:-50,
-        alignItems: 'center', 
+    iconEye: {
+        fontSize: 18,
+        color: colors.azul_black,
+        padding: 10,
+        alignItems: 'center',
     },
     button: {
-        width: '100%',
-        marginTop: 60,
-        paddingHorizontal: 20
-    }
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+        marginBottom: 10,
+        height: 56,
+        width: 200
+    },
+
+    linearGradientBackGround: {
+        flex: 1,
+        paddingLeft: 40,
+        paddingRight: 40,
+        borderRadius: 5
+    },
+
+    confirmText: {
+        fontSize: 20,
+        fontFamily: fonts.text,
+        color: 'white',
+        marginTop: 13
+    },
+
+    gradientButton: {
+        flex: 1,
+        paddingLeft: 50,
+        paddingRight: 50,
+        borderRadius: 10
+    },
 })
